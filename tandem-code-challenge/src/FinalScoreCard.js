1201a5cead563969029ffdee806b460b37e67e86
xoweekday <xoweekday@gmail.com>
2020-11-01 10:22:50 -0600

    delete unused files and remove console.logs

--- tandem-code-challenge/src/FinalScoreCard.js
+++ tandem-code-challenge/src/FinalScoreCard.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Card, CardContent, Grid } from "@material-ui/core";
 
-export const FinalScoreCard = ({finalScore}) => {
-    console.log(finalScore)
+export const FinalScoreCard = ({ finalScore }) => {
   return (
     <Card elevation={1}>
       <CardContent>
         <Grid container wrap="nowrap" spacing={2}>
           <Grid item xs="auto">
-              { `You got ${finalScore} out of 10 questions correct!`}
+            {`You got ${finalScore} out of 10 questions correct!`}
           </Grid>
         </Grid>
       </CardContent>
